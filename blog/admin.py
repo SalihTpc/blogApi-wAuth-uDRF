01b1ab3b4326b0e54dcc7f6665e4d7e5e19bd36c
SalihTpc <salihtopcu@hotmail.com.tr>
2022-03-18 02:20:19 +0300

    slug fields excluded

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from .models import Category, Post, Comment, Like, PostView 
 
-# Register your models here.
-admin.site.register(Category)
-admin.site.register(Post)
+class CategoryAdmin(models.ModelAdmin):
+    exclude = ('slug',)
+
+class PostAdmin(models.ModelAdmin):
+    exclude = ('slug',)
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
 admin.site.register(Comment)
 admin.site.register(Like)
 admin.site.register(PostView)
