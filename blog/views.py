c5e4db93c9aff31002daadf93ae8d5682066bf97
SalihTpc <salihtopcu@hotmail.com.tr>
2022-03-17 15:19:55 +0300

    views added

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import CategorySerializer, PostSerializer
+from .models import Category, Post
 
-# Create your views here.
+class CategoryView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    
+class CategoryDetailView(generics.ListAPIView):
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        post = self.kwargs['category'].title()
+        return Post.objects.filter(category__name=post)
