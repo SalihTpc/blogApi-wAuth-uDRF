afaa368e70fab3d7ff1b52b13469d9997b935ed0
SalihTpc <salihtopcu@hotmail.com.tr>
2022-03-17 15:19:45 +0300

    main urls changed

--- main/urls.py
+++ main/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('__debug__/', include('debug_toolbar.urls')),
+    path('api/', include('blog.urls')),
 ]
 
