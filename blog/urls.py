6722c1b25a0a9b4ca0ffbfd4b8e6a45850d8cc05
SalihTpc <salihtopcu@hotmail.com.tr>
2022-03-21 21:23:17 +0300

    paths added.

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import CategoryView, CategoryDetailView, PostListView
+from .views import CategoryView, CategoryDetailView, PostListView, PostDetailView, CommentView, LikeView, PostViewList
 
 
 urlpatterns = [
     path('list/', PostListView.as_view(), name='postList'),
+    path('list/<int:id>/', PostDetailView.as_view(), name='post-detail'),
+    path('list/<int:id>/comment/', CommentView.as_view(), name='comment'),
+    path('list/<int:id>/like/', LikeView.as_view(), name='like'),
+    path('list/<int:id>/view/', PostViewList.as_view(), name='postView'),
     path('category/list/', CategoryView.as_view(), name='cetagorylist'),
     path('category/<category>/', CategoryDetailView.as_view(), name='details'),
 ]
